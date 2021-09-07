8a6d2dfd8bbc542e81869d17cff01ac3451bd144
Kazuya-Sakashita <kz0508@gmail.com>
2021-09-07 12:16:19 +0900

    add reaction

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+ 
+  get 'chats/show'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  
+  root 'homes#index'
   resources :reactions, only: [:create]
+  resources :matching, only: [:index]
+  get 'chat/:id', to: 'chats#show', as: 'chat'
+  resources :chats, only: [:create]
 end
